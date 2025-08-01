f1afd382b7845f096ef19dd0a2cc36a5fcd47e7b
Anjana Edirisinghe <anjanaape@gmail.com>
2025-08-02 02:12:46 +0530

    db implementation

--- smart-contracts/scripts/petition.js
+++ smart-contracts/scripts/petition.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const Petitions = await ethers.getContractFactory("Petitions");
   const petitions = await Petitions.attach(contractAddress);
 
@@ -15,6 +15,7 @@ async function main() {
       "Less goooooo",
       2
     );
+    
     const receipt1 = await tx1.wait();
 
     let petitionId;
